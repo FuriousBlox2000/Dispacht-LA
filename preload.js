8fcb20d72409cfcda4493681aadff1cc116786a4
FuriousBlox2000 <96319589+FuriousBlox2000@users.noreply.github.com>
2026-02-28 15:39:09 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-    login: (username, password) => ipcRenderer.invoke("login", username, password),
-    loginSuccess: () => ipcRenderer.send("login-success")
-});
